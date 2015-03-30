999e3b5d50a0421c6b8cb02528d1f7f379dab15d
Ádám Gólya <adam.stork@gmail.com>
2015-03-30 22:52:21 +0200

    add getPlayerSize function

--- js/services.js
+++ js/services.js
@@ -3,14 +3,14 @@ clarifyControllers.factory('gamestate', function() {
     var level = 1;
     var numberOfEnemies = 2;
     var maxTime = 10;
-
-
+    var playerSize = 5;
 
     var GameState = {
         levelUp: function () {
             level++;
             numberOfEnemies++;
             maxTime++;
+            playerSize++;
         },
         getLevel: function () {
             return level;
@@ -20,6 +20,9 @@ clarifyControllers.factory('gamestate', function() {
         },
         getMaxTime: function () {
             return maxTime;
+        },
+        getPlayerSize: function () {
+            return playerSize;
         }
     };
 
